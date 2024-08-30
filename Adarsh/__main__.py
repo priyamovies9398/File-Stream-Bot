76a55f11f9c9c9b3201e274feb38ec3c54e16884
PriyaBots <158731094+priyamovies9398@users.noreply.github.com>
2024-08-30 21:08:51 +0530

    Update __main__.py

--- Adarsh/__main__.py
+++ Adarsh/__main__.py
@@ -65,7 +65,7 @@ async def start_services():
     print('\n')
     print('---------------------------------------------------------------------------------------------------------')
     print('---------------------------------------------------------------------------------------------------------')
-    print(' follow me for more such exciting bots! https://github.com/CodeXBots')
+    print(' follow me for more such exciting bots! https://github.com/')
     print('---------------------------------------------------------------------------------------------------------')
     print('\n')
     print('----------------------- Service Started -----------------------------------------------------------------')
@@ -75,7 +75,7 @@ async def start_services():
     if Var.ON_HEROKU:
         print('                        app runnng on =>> {}'.format(Var.FQDN))
     print('---------------------------------------------------------------------------------------------------------')
-    print('Give a star to my repo https://github.com/CodeXBots  also follow me for new bots')
+    print('Give a star to my repo https://github.com/  also follow me for new bots')
     print('---------------------------------------------------------------------------------------------------------')
     await idle()
 
